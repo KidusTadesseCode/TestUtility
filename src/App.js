06b97a9166b2c37efe805aae549d79bfb86adfbd
Kidus Arega <>
2025-02-18 17:22:13 -0500

    Switched from createBrowserRouter to createHashRouter

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import { RouterProvider } from "react-router-dom";
 import PageRoutes from "./PageRoutes";
 import BrowserRouter from "./Global/Outlet/BrowserRouter";
 
-// export default function App() {
-//   return (
-//     <BrowserRouter>
-//       <BrowserRouter router={PageRoutes} />
-//     </BrowserRouter>
-//   );
-// }
-
 export default function App() {
   return <RouterProvider router={BrowserRouter(PageRoutes)} />;
 }
--- src/Global/Outlet/BrowserRouter.js
+++ src/Global/Outlet/BrowserRouter.js
@@ -1,23 +1,7 @@
-import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
-//
 import { createHashRouter } from "react-router-dom";
-// import routes from "./routes";
-import PageRoutes from "../../PageRoutes";
 
 // import { l } from "@zufan_devops/console";
-
-// const BrowserRouter = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Layout />,
-//     children: PageRoutes.map((route) => ({
-//       path: route.path,
-//       element: route.element,
-//     })),
-//   },
-// ]);
-// export default BrowserRouter;
 
 function BrowserRouter(routeList) {
   const routes = routeList.map((route) => ({
